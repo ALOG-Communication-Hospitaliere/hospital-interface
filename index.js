e8ebd0d4e573dbf5a836265263e0e46a46ced761
injok <oumikaba5@gmail.com>
2025-05-18 13:17:35 +0100

    fix

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-document.getElementById('sendBtn').addEventListener('click', function () {
-  const num = document.getElementById('num').value;
-  const type = document.getElementById('type').value;
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('sendBtn').addEventListener('click', function () {
+    const num = document.getElementById('num').value;
+    const type = document.getElementById('type').value;
 
-  // Just printing for now
-  console.log('Numéro de sécurité:', num);
-  console.log('Type choisi:', type);
-
-  // Later: you can use fetch to send this data to your Django API
+    const obj = {
+      numeroSecuriteSociale: num,
+      typeDocument: type
+    };
+     console.log(JSON.stringify(obj) )
+    fetch('http://localhost:6001/api/requests', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(obj) 
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erreur lors de l’envoi de la requête');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Réponse du serveur :', data);
+    })
+    .catch(error => {
+      console.error('Erreur :', error);
+    });
+  });
 });
